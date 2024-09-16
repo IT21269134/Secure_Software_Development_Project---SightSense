d08c4fe81e5ad27e372ed711bc8d006f52089080
K.G.I.H.Chamathka Kumbukgolla <86015142+IT21269134@users.noreply.github.com>
2024-09-16 16:54:01 +0530

    initial changes

---
+++ frontend/src/pages/Clinical_pages/ClinicHome.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import "../../styles/GeneralTest/Clinical/HomePage.css";
+import Card from "../../components/Clinical_components/ClinicCard";
+import "../../styles/GeneralTest/Clinical/NavBar.css";
+import "../../assets/Clinical_assets/search-line.png";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import LogoImage from "../../assets/Clinical_assets/logo.jpg";
+
+function ClinicHome() {
+  const [clinics, setClinics] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          // `http://localhost:4000/Clinics/getAll`
+          `${process.env.REACT_APP_API_URL}/Clinics/getAll`
+        );
+        // if (response.data) {
+          setClinics(response.data);
+        // }
+      } catch (err) {
+        console.log("Error fetching clinics:",err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredClinics = clinics.filter(
+    (clinic) =>
+      // clinic.clinicLocation.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    // clinic.clinicName.toLowerCase().includes(searchQuery.toLowerCase())
+    [clinic.clinicLocation, clinic.clinicName]
+    .join(" ")
+    .toLowerCase()
+    .includes(searchQuery.toLowerCase())
+
+  );
+
+  const handleClinicWebsiteClick = (clinicWebsite) => {
+    const sanitizedUrl = clinicWebsite.startsWith("http")
+    ? clinicWebsite
+    : `https://${clinicWebsite}`; // Ensure URLs are valid and safe
+    window.open(clinicWebsite, "_blank"); // Open the link in a new tab
+  };
+
+  return (
+    <div>
+      <div className="Container">
+        <div className="Inner">
+          <div className="Logo">
+            <Link to="/">
+              <img src={LogoImage} alt="Logo" className="logo" />
+            </Link>
+          </div>
+
+          <div className="SearchBar">
+            <input
+              type="text"
+              placeholder="Search.."
+              className="input"
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+            <button className="search-btn">
+              <i className="ri-search-line"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="Main">
+        <div className="clinic-grid">
+          {filteredClinics.map((clinic, index) => (
+            <Card
+              key={index}
+              clinicId={clinic.id}
+              clinicName={clinic.clinicName}
+              clinicLocation={clinic.clinicLocation}
+              clinicContact={clinic.clinicContact}
+              clinicWebsite={
+                <a
+                  href={clinic.clinicWebsite}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => {
+                    e.preventDefault(); // Prevent default link behavior
+                    handleClinicWebsiteClick(clinic.clinicWebsite);
+                  }}
+                >
+                  {clinic.clinicWebsite}
+                    {/* .replace(/</g, "&lt;").replace(/>/g, "&gt;")} */}
+                </a>
+              }
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClinicHome;
+
